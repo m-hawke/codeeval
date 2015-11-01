4d1257294d27dd709f094ce725616334c24de3f6
Malcolm Hawke <m-hawke@users.noreply.github.com>
2015-11-01 22:30:15 +1100

    Python solutions to problem 200: Sort matrix columns

---
+++ moderate/200_sort_matrix_columns.py
@@ -0,0 +1,9 @@
+import sys
+
+for line in open(sys.argv[1]):
+    print(' | '.join([' '.join([str(x) for x in row]) for row in zip(*sorted(zip(*[[int(x) for x in row.split()] for row in line.split(' | ')])))]))
+
+## Above is a one-liner version of this:
+#    rows = zip(*sorted(zip(*[[int(x) for x in row.split()]
+#                                for row in line.split(' | ')])))
+#    print(' | '.join([' '.join([str(x) for x in row]) for row in rows]))

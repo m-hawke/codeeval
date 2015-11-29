dbe35268f3ec3a22dfb3f5e63a8622aa5c273250
Malcolm Hawke <m-hawke@users.noreply.github.com>
2015-11-29 22:12:15 +1100

    Added a ctypes call to libc's fnmatch. It uses less memory than pure Python, but only works for Python 2 on codeeval's platform.

--- moderate/169_filename_pattern.py
+++ moderate/169_filename_pattern.py
@@ -1,7 +1,19 @@
 import sys
-import fnmatch
+import ctypes
+
+# On codeeval's platform this works in Python 2 only.
+libc = ctypes.CDLL('libc.so.6')
 
 for line in sys.stdin:
     l = line.strip().split()
-    matches = fnmatch.filter(l[1:], l[0])
+    matches = [filename for filename in l[1:]
+                    if libc.fnmatch(l[0], filename) == 0]
     print(' '.join(matches) if matches else '-')
+
+## The following works in Python 2 & 3 but is memory hungry.
+#import fnmatch
+#
+#for line in sys.stdin:
+#    l = line.strip().split()
+#    matches = fnmatch.filter(l[1:], l[0])
+#    print(' '.join(matches) if matches else '-')
